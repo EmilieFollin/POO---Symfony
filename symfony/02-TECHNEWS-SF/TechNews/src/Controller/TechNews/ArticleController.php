4e963d58f682e7e7e7d7bdcc912a1ba88e8f1cf1
EmilieeeeF <EmilieeeeF@users.noreply.github.com>
2018-02-21 10:18:43 +0100

    ajout BDD

---
+++ symfony/02-TECHNEWS-SF/TechNews/src/Controller/TechNews/ArticleController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller\TechNews;
+
+use App\Entity\Article;
+use App\Entity\Auteur;
+use App\Entity\Categorie;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class ArticleController extends Controller
+{
+    /**
+     * Demonstration, Ajouter un article avec Doctrine
+     * @Route("/article", name="article")
+     */
+    public function index()
+    {
+        #Création de la categorie
+        $categorie = new Categorie();
+        $categorie->setLibelle('Business');
+
+        #Création de auteur
+        $auteur = new Auteur();
+        $auteur->setPrenom('Emilie');
+        $auteur->setNom('Follin');
+        $auteur->setEmail('emiliefollin@gmail.com');
+        $auteur->setPassword('test');
+
+        #Création de notre article
+        $article = new Article();
+        $article->setTitre('Ceci est un titre');
+        $article->setContenu('ceci est un contenu');
+        $article->setFeaturedimage('3.jpg');
+        $article->setSpecial(0);
+        $article->setSpotlight(1);
+
+        #Associe une categorie et un auteur a notre article
+        $article->setCategorie($categorie);
+        $article->setAuteur($auteur);
+
+        #On sauvegarde le tout en BDD
+        #em : Entity Manager
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($categorie);
+        $em->persist($article);
+        $em->persist($auteur);
+        $em->flush();
+        #Flush : sauvegarde
+
+
+        #Retour de la vue
+        return new Response('Nouvel article ajouté avec ID : ' .
+            $article->getId() . ' et la nouvelle catégorie : ' .
+            $categorie->getLibelle() . ' de Auteur : ' .
+            $auteur->getPrenom() );
+    }
+}

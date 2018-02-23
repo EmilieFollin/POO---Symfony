e57aa09b625ec94c6b33e2e50ac3714e32cd3e4a
EmilieFollin <EmilieFollin@users.noreply.github.com>
2018-02-23 14:50:36 +0100

    MAj

--- symfony/rappel/Eleve.php
+++ symfony/rappel/Eleve.php
@@ -13,8 +13,19 @@ class Eleve
     #propriété
     public $_nom = "FOLLIN",
            $_prenom = "Emilie";
+    #proteger : visible a l'interieur de la classe mais erreur d'affichage
+    #private =  seulement a l'interieur de la classe
+    #proteged = seulemtn a l'interieur de la classe et ceux qui hérite
+    #public = accessible depuis l'exterieur
+    protected $email = "emiliefollin@gmail.com";
+
+    #methode
+    public function getNomComplet(){
+        echo $this->_prenom. ' '. $this->_nom;
+    }
 
 }
 
 $mon_eleve = new Eleve();
-echo $mon_eleve->_prenom;
+echo $mon_eleve->_prenom. '<br>';
+$mon_eleve->getNomComplet(). '<br>';
